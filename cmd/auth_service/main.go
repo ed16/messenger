1cf288bf485e10c03924ac8a8b13f75f86579ddd
ed16 <e.zaynullin@gmail.com>
2024-06-03 11:18:16 +0200

    Move middleware to the separate package

--- cmd/auth_service/main.go
+++ cmd/auth_service/main.go
@@ -62,27 +62,3 @@ func main() {
 	}
 	// Server gracefully shutdown
 }
-
-// Middleware to log each request and its response status code
-func logRequests(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Serve the request and record the response status code
-		rw := &responseWriter{ResponseWriter: w}
-		next.ServeHTTP(rw, r)
-
-		// Log the request and response status code
-		log.Printf("%s %s - Response status code: %d", r.Method, r.URL.Path, rw.status)
-	})
-}
-
-// Custom ResponseWriter to capture the status code
-type responseWriter struct {
-	http.ResponseWriter
-	status int
-}
-
-// Override WriteHeader to capture the status code
-func (rw *responseWriter) WriteHeader(code int) {
-	rw.status = code
-	rw.ResponseWriter.WriteHeader(code)
-}
